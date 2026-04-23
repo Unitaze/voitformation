96342b2c64fcd8eb6577ddc2ba78c17e3ff56ada
Unitaze <maximalcrazy88@gmail.com>
2026-04-23 23:37:56 +0300

    All file must drop

---
+++ docker-hub/rygota.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Вот этот текст мы будем менять в Гитхабе, чтобы проверить обновление
+    return "<h1>Привет! Я работаю на отьебись, как и все здесь.</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
